6831143a089c856abce46b7f1e7f349f666841b8
Ali Khankhel <alikhan7@users.noreply.github.com>
2017-05-29 12:38:17 +0500

    Add files via upload

---
+++ jTableSimple.php
@@ -0,0 +1,61 @@
+<html>
+  <head>
+
+    <link href="themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
+	<link href="Scripts/jtable/themes/lightcolor/red/jtable.css" rel="stylesheet" type="text/css" />
+	
+	<script src="scripts/jquery-1.6.4.min.js" type="text/javascript"></script>
+    <script src="scripts/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
+    <script src="Scripts/jtable/jquery.jtable.js" type="text/javascript"></script>
+	
+  </head>
+  <body>
+	<div id="PeopleTableContainer" style="width: 900px;"></div>
+	<script type="text/javascript">
+
+		$(document).ready(function () {
+
+		    //Prepare jTable
+			$('#PeopleTableContainer').jtable({
+				title: 'Employes of Codematics',
+				actions: {
+					listAction: 'PersonActions.php?action=list',
+					createAction: 'PersonActions.php?action=create',
+					updateAction: 'PersonActions.php?action=update',
+					deleteAction: 'PersonActions.php?action=delete'
+				},
+				fields: {
+					PersonId: {
+						key: true,
+						create: false,
+						edit: false,
+						title:'No',
+						list: true
+					},
+					Name: {
+						title: 'Employe Name',
+						width: '40%'
+					},
+					Age: {
+						title: 'Age',
+						width: '20%'
+					},
+					RecordDate: {
+						title: 'Record date',
+						width: '30%',
+						type: 'date',
+						create: true,
+						edit: true
+					}
+				}
+			});
+
+			//Load person list from server
+			$('#PeopleTableContainer').jtable('load');
+
+		});
+
+	</script>
+ 
+  </body>
+</html>
